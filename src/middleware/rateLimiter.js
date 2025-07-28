a7266fc747ca6918bd4604e5b2958db2825e76cc
domems <domems@gmail.com>
2025-07-28 10:41:58 +0100

    RateLimiter

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,23 +1,23 @@
 import ratelimit from "../config/upstash.js";
 
-const rateLimiter = async(req,res,nest) => {
+const rateLimiter = async (req, res, next) => {
+  try {
+    const { success } = await ratelimit.limit("my-rate-limit");
 
-    try {
-        const {success} = await ratelimit.limit("my-rate-limit");
-
-        if (!success) {
-            return res.status(429).json({
-                message:"Too many requests, please try again later :)",
-            });
-        }
-
-        next();
-
-    } catch (error) {
-        console.log("Rate limit error", error);
-        next(error);
+    if (!success) {
+      return res.status(429).json({
+        message: "Too many requests, please try again later :)",
+      });
     }
+
+    next(); // continua para o próximo middleware
+
+  } catch (error) {
+    console.log("Rate limit error", error);
+    next(error); // passa o erro para o Express
+  }
 };
 
 export default rateLimiter;
+
 

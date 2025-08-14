ebe64c4175d5078ffac37a36b8aa7069a64126eb
domems <domems@gmail.com>
2025-08-14 23:46:05 +0100

    Admin-Search-Miners

--- src/routes/statusRoutes.js
+++ src/routes/statusRoutes.js
@@ -2,7 +2,8 @@ import express from "express";
 import { getMinerStatus } from "../controllers/statusController.js";
 
 const router = express.Router();
-router.get("/miners/:minerId/status", getMinerStatus);
+router.get("/miners/:id/status", getMinerStatus);
+router.get("/miners/status", getMinersStatusBatch);
 
 export default router;
 

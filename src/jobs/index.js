5e4d96ed578c7fcb885a7355bdad7867605f2ec0
domems <domems@gmail.com>
2025-09-26 16:09:26 +0100

    MiningDutch Api

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -2,7 +2,7 @@ import { startUptimeViaBTC } from "./uptimeViaBTC.js";
 import { startUptimeLTCPool } from "./uptimeLiteCoinPool.js";
 import { startUptimeBinance } from "./uptimeBinance.js";
 import { startUptimeF2Pool } from "./uptimeF2Pool.js";
-import { startUptimeMiningDutch } from "./jobs/uptimeMiningDutch.js";
+import { startUptimeMiningDutch } from "./uptimeMiningDutch.js";
 
 
 let started = false;
@@ -20,7 +20,7 @@ export function startAllJobs() {
   startUptimeBinance();
   startUptimeF2Pool();
   startUptimeMiningDutch();
-  
+
   // (opcional) fecho mensal
   import("./monthlyClose.js")
     .then((m) => m?.startMonthlyClose?.())
